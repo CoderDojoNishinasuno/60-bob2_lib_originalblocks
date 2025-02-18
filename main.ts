3c98f2e4396089e24a8a62701e438e81c01b688c
CoderDojo西那須野 <131562680+CoderDojoNishinasuno@users.noreply.github.com>
2025-02-18 23:12:22 +0900

    不足の関数を追加。全部揃えた。

--- main.ts
+++ main.ts
@@ -1,37 +1,43 @@
-// % block="うしろにすすむ"
+// % block="うしろに進む"
+// % group="動き"
 function backword () {
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.digitalWritePin(DigitalPin.P14, 1)
     pins.digitalWritePin(DigitalPin.P15, 1)
     pins.digitalWritePin(DigitalPin.P16, 0)
 }
-// % block="ひだりて|number $angle"
+// % block="左手うごかす %angle 度"
+// % group="うで"
 function leftHand (angle: number) {
     servos.P2.setAngle(120 - angle)
 }
-// % block="とめる"
+// % block="止める"
+// % group="動き"
 function stop () {
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P15, 0)
     pins.digitalWritePin(DigitalPin.P16, 0)
 }
-// % block="みぎまわる|number $angle"
+// % block="右に回る %t 秒"
+// % group="動き"
 function turnRight (t: number) {
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P15, 1)
     pins.digitalWritePin(DigitalPin.P16, 0)
-    basic.pause(t)
+    basic.pause(t * 1000)
 }
-// % block="まえにすすむ"
+// % block="まえに進む"
+// % group="動き"
 function forward () {
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P15, 0)
     pins.digitalWritePin(DigitalPin.P16, 1)
 }
-// % block="みぎて|number $angle"
+// % block="右手うごかす %angle 度"
+// % group="うで"
 function rightHand (angle: number) {
     servos.P1.setAngle(60 - angle)
 }
