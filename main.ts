1f97baffc5bc9ed9b2ecb3c17f4ebab84b833461
CoderDojo西那須野 <131562680+CoderDojoNishinasuno@users.noreply.github.com>
2025-02-18 23:39:23 +0900

    修正

--- main.ts
+++ main.ts
@@ -1,87 +1,72 @@
-//% color=#F0a020 icon="\uf001" block="BOB V2" weight=100
-//% groups="['動き','うで']"
-
-
-    //% block="止める"
-    //% group="動き"
-     function stop() {
-        pins.digitalWritePin(DigitalPin.P13, 0)
-        pins.digitalWritePin(DigitalPin.P14, 0)
-        pins.digitalWritePin(DigitalPin.P15, 0)
-        pins.digitalWritePin(DigitalPin.P16, 0)
+// % block="うしろに進む"
+// % group="動き"
+function backword () {
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+}
+// % block="左に回る %t 秒"
+// % group="動き"
+function turnLeft (t: number) {
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.pause(t * 1000)
+}
+// % block="左手うごかす %angle 度"
+// % group="うで"
+function leftHand (angle: number) {
+    servos.P2.setAngle(120 - angle)
+}
+// % block="左手をふる %num 回"
+// % group="うで"
+function waveLeftHand (num: number) {
+    for (let index = 0; index < num; index++) {
+        leftHand(-30)
+        basic.pause(200)
+        leftHand(30)
+        basic.pause(200)
     }
-
-    //% block="右手うごかす %angle 度"
-    //% group="うで"
-     function rightHand(angle: number) {
-        servos.P1.setAngle(60 - angle)
+}
+// % block="止める"
+// % group="動き"
+function stop () {
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+}
+// % block="右に回る %t 秒"
+// % group="動き"
+function turnRight (t: number) {
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    basic.pause(t * 1000)
+}
+// % block="右手をふる %num 回"
+// % group="うで"
+function waveRightHand (num: number) {
+    for (let index = 0; index < num; index++) {
+        rightHand(-30)
+        basic.pause(200)
+        rightHand(30)
+        basic.pause(200)
     }
-
-    //% block="左手うごかす %angle 度"
-    //% group="うで"
-     function leftHand(angle: number) {
-        servos.P2.setAngle(120 - angle)
-    }
-
-    //% block="うしろに進む"
-    //% group="動き"
-     function backword() {
-        pins.digitalWritePin(DigitalPin.P13, 0)
-        pins.digitalWritePin(DigitalPin.P14, 1)
-        pins.digitalWritePin(DigitalPin.P15, 1)
-        pins.digitalWritePin(DigitalPin.P16, 0)
-    }
-
-    //% block="まえに進む"
-    //% group="動き"
-     function forward() {
-        pins.digitalWritePin(DigitalPin.P13, 1)
-        pins.digitalWritePin(DigitalPin.P14, 0)
-        pins.digitalWritePin(DigitalPin.P15, 0)
-        pins.digitalWritePin(DigitalPin.P16, 1)
-    }
-
-    //% block="右に回る %t 秒"
-    //% group="動き"
-     function turnRight(t: number) {
-        pins.digitalWritePin(DigitalPin.P13, 1)
-        pins.digitalWritePin(DigitalPin.P14, 0)
-        pins.digitalWritePin(DigitalPin.P15, 1)
-        pins.digitalWritePin(DigitalPin.P16, 0)
-        basic.pause(t * 1000)
-        stop()
-    }
-
-    //% block="左に回る %t 秒"
-    //% group="動き"
-     function turnLeft(t: number) {
-        pins.digitalWritePin(DigitalPin.P13, 0)
-        pins.digitalWritePin(DigitalPin.P14, 1)
-        pins.digitalWritePin(DigitalPin.P15, 0)
-        pins.digitalWritePin(DigitalPin.P16, 1)
-        basic.pause(t * 1000)
-        stop()
-    }
-
-    //% block="右手をふる %num 回"
-    //% group="うで"
-     function waveRightHand(num: number) {
-        for (let index = 0; index < num; index++) {
-            rightHand(-30)
-            basic.pause(200)
-            rightHand(30)
-            basic.pause(200)
-        }
-    }
-
-    //% block="左手をふる %num 回"
-    //% group="うで"
-     function waveLeftHand(num: number) {
-        for (let index = 0; index < num; index++) {
-            leftHand(-30)
-            basic.pause(200)
-            leftHand(30)
-            basic.pause(200)
-        }
-    }
-
+}
+// % block="まえに進む"
+// % group="動き"
+function forward () {
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+}
+// % block="右手うごかす %angle 度"
+// % group="うで"
+function rightHand (angle: number) {
+    servos.P1.setAngle(60 - angle)
+}
